07fe0d607787b3a2ef63c01da3b57660ea8a6029
Sergio Camacho <sergio.camachoo@misena.edu.co>
2021-08-03 16:11:46 -0500

    Correcciones 3

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,8 +15,6 @@
     .mb-5.p-5.figure-introduccion-2
       p.mb-0 Para la elaboración de este componente, se abordaron varios autores conocidos en procesamiento, fabricación y ensamble, de quienes se han citado y referenciado conceptos y ejemplos para los fines educativos de esta materia, en el entendido que el conocimiento es social y, por lo tanto, es para usarlo por quienes necesitan adquirirlo. Se espera que este documento sea útil para todos, aprendices y lectores en general, que estén interesados en acercarse a asuntos básicos del desarrollo de colecciones para la industria de la moda.
 
-    p.mb-5 Para la elaboración de este componente, se abordaron varios autores conocidos en procesamiento, fabricación y ensamble, de quienes se han citado y referenciado conceptos y ejemplos para los fines educativos de esta materia, en el entendido que el conocimiento es social y, por lo tanto, es para usarlo por quienes necesitan adquirirlo. Se espera que este documento sea útil para todos, aprendices y lectores en general, que estén interesados en acercarse a asuntos básicos del desarrollo de colecciones para la industria de la moda.
-
 </template>
 
 <script>
